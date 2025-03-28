028fb2275231daa0acb439d5ad60fb072d7227d9
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-03-28 08:49:07 +0100

    Carpetas e inicio de main-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { MainPageComponent } from './pages/main-page/main-page.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:MainPageComponent,
+  }
+];
---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chatbot-main-page',
+  imports: [],
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+
+  
+ }
